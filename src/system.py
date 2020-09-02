cb8db8ed2bdaaf7da0bf66598da927e5c5dcfd8a
Andrew Rooney <arrooney@ualberta.ca>
2020-09-02 15:10:13 -0700

    feat: instant housekeeping

--- src/system.py
+++ src/system.py
@@ -141,6 +141,25 @@ class SystemValues(object):
                                 'timeLeftInS': '>u4'
                             }
                         }
+                    },
+                    'EPS_HARD_RESET': {
+                        'subPort': 2,
+                        'inoutInfo': {
+                            'args': ['>u2'], # 17767
+                            'returns': {
+                                'err': '>b'
+                            }
+                        }
+                    },
+                    # Note: soft reset is done VIA CSP services - refer to docs
+                    'PAUSE_EPS_DEPLOYMENT_ACTION': {
+                        'subPort': 8,
+                        'inoutInfo': {
+                            'args': ['>B', '>u4'], # group, time
+                            'returns': {
+                                'err': '>b'
+                            }
+                        }
                     }
                 }
             },
@@ -149,6 +168,138 @@ class SystemValues(object):
                 # EPS SPECIFIC
                 'port': 13, # EPS remote CLI uses port 13 unless Otherwise specified
                 'subservice': {
+                    'GENERAL_TELEMETERY': {
+                        'subPort': 0,
+                        'inoutInfo': {
+                            'args': None,
+                            'returns': {
+                                'err': '>b',
+                                'timestamp': '>u4',
+                                'uptimeInS': '>f8',
+                                'gs_wdt_time_left_s': '>u4',
+                                'counter_wdt_gs': '>u4',
+                                'mpptConverterVoltage0_mV': '>u2',
+                                'mpptConverterVoltage1_mV': '>u2',
+                                'mpptConverterVoltage2_mV': '>u2',
+                                'mpptConverterVoltage3_mV': '>u2',
+                                'curSolarPanels0_mA': '>u2',
+                                'curSolarPanels1_mA': '>u2',
+                                'curSolarPanels2_mA': '>u2',
+                                'curSolarPanels3_mA': '>u2',
+                                'curSolarPanels4_mA': '>u2',
+                                'curSolarPanels5_mA': '>u2',
+                                'curSolarPanels6_mA': '>u2',
+                                'curSolarPanels7_mA': '>u2',
+                                'vBatt_mV': '>u2',
+                                'curSolar_mA': '>u2',
+                                'curBattIn_mA': '>u2',
+                                'curBattOut_mA': '>u2',
+                                'curOutput0_mA': '>u2',
+                                'curOutput1_mA': '>u2',
+                                'curOutput2_mA': '>u2',
+                                'curOutput3_mA': '>u2',
+                                'curOutput4_mA': '>u2',
+                                'curOutput5_mA': '>u2',
+                                'curOutput6_mA': '>u2',
+                                'curOutput7_mA': '>u2',
+                                'curOutput8_mA': '>u2',
+                                'curOutput9_mA': '>u2',
+                                'curOutput10_mA': '>u2',
+                                'curOutput11_mA': '>u2',
+                                'curOutput12_mA': '>u2',
+                                'curOutput13_mA': '>u2',
+                                'curOutput14_mA': '>u2',
+                                'curOutput15_mA': '>u2',
+                                'curOutput16_mA': '>u2',
+                                'curOutput17_mA': '>u2',
+                                'AOcurOutput0_mA': '>u2',
+                                'AOcurOutput1_mA': '>u2',
+                                'outputConverterVoltage0': '>u2',
+                                'outputConverterVoltage1': '>u2',
+                                'outputConverterVoltage2': '>u2',
+                                'outputConverterVoltage3': '>u2',
+                                'outputConverterVoltage4': '>u2',
+                                'outputConverterVoltage5': '>u2',
+                                'outputConverterVoltage6': '>u2',
+                                'outputConverterVoltage7': '>u2',
+                                'outputConverterState': '>B', # 4 bits!
+                                'outputStatus': '>u4',
+                                'outputFaultStatus': '>u4',
+                                'outputOnDelta0': '>u2',
+                                'outputOnDelta1': '>u2',
+                                'outputOnDelta2': '>u2',
+                                'outputOnDelta3': '>u2',
+                                'outputOnDelta4': '>u2',
+                                'outputOnDelta5': '>u2',
+                                'outputOnDelta6': '>u2',
+                                'outputOnDelta7': '>u2',
+                                'outputOnDelta8': '>u2',
+                                'outputOnDelta9': '>u2',
+                                'outputOnDelta10': '>u2',
+                                'outputOnDelta11': '>u2',
+                                'outputOnDelta12': '>u2',
+                                'outputOnDelta13': '>u2',
+                                'outputOnDelta14': '>u2',
+                                'outputOnDelta15': '>u2',
+                                'outputOnDelta16': '>u2',
+                                'outputOnDelta17': '>u2',
+                                'outputOffDelta0': '>u2',
+                                'outputOffDelta1': '>u2',
+                                'outputOffDelta2': '>u2',
+                                'outputOffDelta3': '>u2',
+                                'outputOffDelta4': '>u2',
+                                'outputOffDelta5': '>u2',
+                                'outputOffDelta6': '>u2',
+                                'outputOffDelta7': '>u2',
+                                'outputOffDelta8': '>u2',
+                                'outputOffDelta9': '>u2',
+                                'outputOffDelta10': '>u2',
+                                'outputOffDelta11': '>u2',
+                                'outputOffDelta12': '>u2',
+                                'outputOffDelta13': '>u2',
+                                'outputOffDelta14': '>u2',
+                                'outputOffDelta15': '>u2',
+                                'outputOffDelta16': '>u2',
+                                'outputOffDelta17': '>u2',
+                                'outputFaultCount0': '>u2',
+                                'outputFaultCount1': '>u2',
+                                'outputFaultCount2': '>u2',
+                                'outputFaultCount3': '>u2',
+                                'outputFaultCount4': '>u2',
+                                'outputFaultCount5': '>u2',
+                                'outputFaultCount6': '>u2',
+                                'outputFaultCount7': '>u2',
+                                'outputFaultCount8': '>u2',
+                                'outputFaultCount9': '>u2',
+                                'outputFaultCount10': '>u2',
+                                'outputFaultCount11': '>u2',
+                                'outputFaultCount12': '>u2',
+                                'outputFaultCount13': '>u2',
+                                'outputFaultCount14': '>u2',
+                                'outputFaultCount15': '>u2',
+                                'outputFaultCount16': '>u2',
+                                'outputFaultCount17': '>u2',
+                                'temp0_c': '>b',
+                                'temp1_c': '>b',
+                                'temp2_c': '>b',
+                                'temp3_c': '>b',
+                                'temp4_c': '>b',
+                                'temp5_c': '>b',
+                                'temp6_c': '>b',
+                                'temp7_c': '>b',
+                                'temp8_c': '>b',
+                                'temp9_c': '>b',
+                                'temp10_c': '>b',
+                                'temp11_c': '>b',
+                                'temp12_c': '>b',
+                                'temp13_c': '>b',
+                                'battState': '>B',
+                                'mpptMode': '>B',
+                                'battHeaterMode': '>B',
+                                'battHeaterState': '>B'
+                            }
+                        }
+                    }
                     # ALL the EPS CLI commands:
                     'SET_TELEMETERY_PERIOD': {
                         'what': 'Set telemetery collection period on EPS',
