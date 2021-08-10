456b5cc88a5e8f77cc52c612431c77d917c9e4b4
Robert <taylorr82@mymacewan.ca>
2021-08-10 13:22:25 -0600

    make i2c use new driver

--- equipment_handler/include/i2c.c
+++ equipment_handler/include/i2c.c
@@ -8,58 +8,14 @@
 #include "i2c.h"
 
 #include "HL_i2c.h"
+#include "i2c_io.h"
 
 void i2c_sendCommand(uint8_t length, char* start, char* response,
                      uint8_t addr) {
+  // TODO: make this use error code return
   i2cBASE_t* regset = i2cREG1;
 
-  i2cSetSlaveAdd(regset, addr);
-  i2cSetDirection(regset, I2C_TRANSMITTER);
-  i2cSetBaudrate(regset, 400);  // Hardcoded
-  i2cSetCount(regset, length);
-  i2cSetMode(regset, I2C_MASTER);
-  i2cSetStop(regset);
-  i2cSetStart(regset);
+  i2c_Send(regset, addr, length, start);
 
-  while (i2cIsBusBusy(regset) == true)
-    ;  // This line is critical
-  i2cSend(regset, length, start);
-
-  /* Wait until Bus Busy is cleared */
-  while (i2cIsBusBusy(regset) == true)
-    ;
-
-  /* Wait until Stop is detected */
-  while (i2cIsStopDetected(regset) == 0)
-    ;
-
-  /* Clear the Stop condition */
-  i2cClearSCD(regset);
-
-  /* Change to receive mode */
-
-  i2cSetSlaveAdd(regset, addr);
-  /* Set direction to receiver */
-  i2cSetDirection(regset, I2C_RECEIVER);
-  i2cSetCount(regset, 100);
-  /* Set mode as Master */
-  i2cSetMode(regset, I2C_MASTER);
-  i2cSetStop(regset);
-  /* Transmit Start Condition */
-  i2cSetStart(regset);
-
-  while (i2cIsBusBusy(regset) == true)
-    ;
-  i2cReceive(regset, 100, response);
-
-  /* Wait until Bus Busy is cleared */
-  while (i2cIsBusBusy(regset) == true)
-    ;
-
-  /* Wait until Stop is detected */
-  while (i2cIsStopDetected(regset) == 0)
-    ;
-
-  /* Clear the Stop condition */
-  i2cClearSCD(regset);
+  i2c_Receive(regset, addr, 100, response);
 }
