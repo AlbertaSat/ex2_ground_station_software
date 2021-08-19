9e2414650099c2d8764fbb0361449def76362bb3
Arash Yazdani <yazdania@ualberta.ca>
2021-08-19 15:19:30 -0600

    Fix i2c.h declaration

--- equipment_handler/include/i2c.h
+++ equipment_handler/include/i2c.h
@@ -10,10 +10,10 @@
 #define I2C_SPEED 400
 #define I2C_BUS_REG i2cREG1
 
-void i2c_sendCommand(uint8_t addr, uint8_t * command, uint8_t length);
+void i2c_sendCommand(uint8_t addr, char * command, uint8_t length);
 
-void i2c_receiveResponse(uint8_t addr, uint8_t * response, uint8_t length);
+void i2c_receiveResponse(uint8_t addr, char * response, uint8_t length);
 
-void i2c_sendAndReceive(uint8_t addr, uint8_t * command, uint8_t command_len, uint8_t * response, uint8_t response_len);
+void i2c_sendAndReceive(uint8_t addr, char * command, uint8_t command_len, char * response, uint8_t response_len);
 
 #endif /* i2c_H */
