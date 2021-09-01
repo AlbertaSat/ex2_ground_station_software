2d0ff8080c789699461a666d5c0b5dff928ab97d
Thomas <ganley@ualberta.ca>
2021-09-01 10:07:48 -0600

    UART renaming and fixing memory bug from rssi in some commands

--- equipment_handler/include/uhf_uart.h
+++ equipment_handler/include/uhf_uart.h
@@ -12,21 +12,22 @@
  * GNU General Public License for more details.
  */
 /**
- * @file uart.c
+ * @file uart.h
  * @author Thomas Ganley
  * @date 2021-08-23
  */
 
-#include "HL_sci.h"
-#include "uart.h"
+#ifndef EX2_HAL_EX2_UHF_SOFTWARE_EQUIPMENT_HANDLER_INCLUDE_UHF_UART_H_
+#define EX2_HAL_EX2_UHF_SOFTWARE_EQUIPMENT_HANDLER_INCLUDE_UHF_UART_H_
 
-void uart_send(uint32_t length, uint8_t * data){
-    sciSetBaudrate(sciREG2, 115200);
-    sciSend(sciREG2, length, data);
-}
+#include <stdbool.h>
 
-void uart_sendAndReceive(uint32_t command_length, uint8_t * command, uint32_t answer_length, uint8_t * ans){
-    sciSetBaudrate(sciREG2, 115200);
-    sciSend(sciREG2, command_length, command);
-    sciReceive(sciREG2, answer_length, ans);
-}
+bool uhf_enter_direct_hardware_mode();
+
+void uhf_exit_direct_hardware_mode();
+
+int uhf_direct_send(uint32_t length, uint8_t * data);
+
+int uhf_direct_sendAndReceive(uint32_t command_length, uint8_t * command, uint32_t answer_length, uint8_t * ans);
+
+#endif /* EX2_HAL_EX2_UHF_SOFTWARE_EQUIPMENT_HANDLER_INCLUDE_UHF_UART_H_ */
