f7c153b9ebf56e6f50285aa28d35125989f6d0d2
Collin Cupido <ccupido@ualberta.ca>
2022-02-03 18:22:46 -0800

    Added genericRead fix as well now.

--- main.c
+++ main.c
@@ -4,21 +4,30 @@
 #include "uTransceiver.h"
 #include "uhf.h"
 
+int main(int argc, char *argv[]) {
+    // For example, sending command 1 with frequency parameter.
+    uint8_t code = 1;
+    uint32_t freq = 437875000;
+
+    UHF_return ret_val = UHF_genericWrite(code, &freq);
+    return 0;
+}
+
 // int main(int argc, char *argv[]) {
 //     // For example, sending command 1 with frequency parameter.
 //     uint8_t code = 1;
 //     uint32_t freq = 437875000;
 
-//     UHF_return ret_val = UHF_genericWrite(code, &freq);
+//     UHF_return ret_val = UHF_genericRead(code, &freq);
 //     return 0;
 // }
 
-int main(int argc, char *argv[]) {
-    // Test from Josh
-    uint8_t code = 0;
-    uint8_t scw[12] = {0, 0b11, 0, 0b101, 0, 0, 1, 0, 0, 0, 1, 1};
+// int main(int argc, char *argv[]) {
+//     // Test from Josh
+//     uint8_t code = 0;
+//     uint8_t scw[12] = {0, 0b11, 0, 0b101, 0, 0, 1, 0, 0, 0, 1, 1};
 
-    UHF_return ret_val = UHF_genericWrite(code, scw);
-    //UHF_return ret_val = HAL_UHF_getFreq(465000000);
-    return 0;
-}
+//     UHF_return ret_val = UHF_genericWrite(code, scw);
+//     //UHF_return ret_val = HAL_UHF_getFreq(465000000);
+//     return 0;
+// }
