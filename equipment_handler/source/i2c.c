2d600618237594c796b0f373ec8e56c25869f647
tganley <58645798+tganley@users.noreply.github.com>
2022-02-15 16:50:57 -0700

    Match return types in i2c to function type

--- equipment_handler/source/i2c.c
+++ equipment_handler/source/i2c.c
@@ -33,16 +33,16 @@ static void uhf_pipe_timer_callback(TimerHandle_t xTimer) {
 UHF_return uhf_i2c_init(void) {
     uTransceiver_semaphore = xSemaphoreCreateMutex();
     if (uTransceiver_semaphore == NULL) {
-        return false;
+        return U_I2C_FAIL;
     }
     xSemaphoreGive(uTransceiver_semaphore);
 
     uTransceiverPipe_timer =
         xTimerCreate("uTransceiverPipe", pdMS_TO_TICKS(100), pdFALSE, NULL, uhf_pipe_timer_callback);
     if (uTransceiverPipe_timer == NULL) {
-        return false;
+        return U_I2C_FAIL;
     }
-    return true;
+    return U_I2C_SUCCESS;
 }
 
 bool uhf_is_busy(void) {
@@ -56,19 +56,19 @@ bool uhf_is_busy(void) {
 UHF_return i2c_prepare_for_pipe_mode(uint32_t timeout_ms) {
     if (xSemaphoreTake(uTransceiver_semaphore, 0) != pdTRUE) {
         // the UHF must already be in pipe mode.
-        return false;
+        return U_I2C_FAIL;
     }
 
     const int timer_cushion_ms = 1000; // a little extra time to make sure the timer comes after pipe mode expires.
     if (xTimerChangePeriod(uTransceiverPipe_timer, pdMS_TO_TICKS(timeout_ms + timer_cushion_ms), 0) != pdPASS) {
         // failed to change timer period
-        return false;
+        return U_I2C_FAIL;
     }
     if (xTimerStart(uTransceiverPipe_timer, 0) != pdPASS) {
         // failed to start timer
-        return false;
+        return U_I2C_FAIL;
     }
-    return true;
+    return U_I2C_SUCCESS;
 }
 
 void uhf_pipe_timer_reset_from_isr(BaseType_t *xHigherPriorityTaskWoken) {
@@ -77,43 +77,41 @@ void uhf_pipe_timer_reset_from_isr(BaseType_t *xHigherPriorityTaskWoken) {
 
 UHF_return i2c_sendCommand(uint8_t addr, char *command, uint8_t length) {
     if (xSemaphoreTake(uTransceiver_semaphore, 0) == pdTRUE) {
+        // TODO: Fix error return once I2C send returns properly
         bool result = (i2c_Send(UHF_I2C, addr, length, command) == I2C_OK);
         xSemaphoreGive(uTransceiver_semaphore);
         return result;
     }
-    return false;
-
-    // TODO: Reset I2C speed to default once the UHF I2C is done
+    return U_I2C_FAIL;
 }
 
 UHF_return i2c_receiveResponse(uint8_t addr, char *response, uint8_t length) {
     if (xSemaphoreTake(uTransceiver_semaphore, 0) == pdTRUE) {
+        // TODO: Fix error return once I2C send returns properly
         bool result = (i2c_Receive(UHF_I2C, addr, length, response) == I2C_OK);
         xSemaphoreGive(uTransceiver_semaphore);
         return result;
     }
-    return false;
-
-    // TODO: Reset I2C speed to default once the UHF I2C is done
+    return U_I2C_FAIL;
 }
 
 UHF_return i2c_sendAndReceive(uint8_t addr, char *command, uint8_t command_len, char *response, uint8_t response_len) {
     if (xSemaphoreTake(uTransceiver_semaphore, 0) == pdTRUE) {
         if (i2c_Send(UHF_I2C, addr, command_len, command) != I2C_OK) {
             xSemaphoreGive(uTransceiver_semaphore);
-            return false;
+            return U_I2C_FAIL;
         }
         xSemaphoreGive(uTransceiver_semaphore);
     }
     if (xSemaphoreTake(uTransceiver_semaphore, 0) == pdTRUE) {
         if (i2c_Receive(UHF_I2C, addr, response_len, response) != I2C_OK) {
             xSemaphoreGive(uTransceiver_semaphore);
-            return false;
+            return U_I2C_FAIL;
         }
         xSemaphoreGive(uTransceiver_semaphore);
-        return true;
+        return U_I2C_SUCCESS;
     }
-    return false;
+    return U_I2C_FAIL;
 
 }
 
@@ -124,16 +122,16 @@ UHF_return i2c_sendAndReceivePIPE(uint8_t addr, char *command, uint8_t command_l
     if (i2c_prepare_for_pipe_mode(1000 * pipe_timeout)) {
         if (i2c_Send(UHF_I2C, addr, command_len, command) != I2C_OK) {
             xSemaphoreGive(uTransceiver_semaphore);
-            return false;
+            return U_I2C_FAIL;
         }
         if (i2c_Receive(UHF_I2C, addr, response_len, response) != I2C_OK) {
             xSemaphoreGive(uTransceiver_semaphore);
-            return false;
+            return U_I2C_FAIL;
         }
-        return true;
+        return U_I2C_SUCCESS;
     } else {
         ex2_log("Error preparing for pipe mode.");
     }
     xSemaphoreGive(uTransceiver_semaphore);
-    return false;
+    return U_I2C_FAIL;
 }
