c5b9280f1044b5fa3f2d1102b31a3f3dfa7a0086
Daniel Sacro <dbsacro@gmail.com>
2022-03-01 12:35:16 -0700

    Added basic test cases for the dfgm

---
+++ test/test_dfgm.py
@@ -0,0 +1,31 @@
+'''  to run > yarn run:test_dfgm -I uart -d /dev/ttyUSB0 '''
+
+import numpy as np
+
+import sys
+from os import path
+sys.path.append("./test")
+
+from testLib import testLib as test
+
+import time
+
+test = test() #call to initialize local test class
+
+def testAllCommandsToOBC():
+    # Checking basic functionality
+    test.sendAndExpect('obc.dfgm.dfgm_run(5)', {'err': 0})
+    time.sleep(6) # Wait for first command to end
+    test.sendAndExpect('obc.dfgm.dfgm_start', {'err': 0})
+    time.sleep(5) # Let the DFGM run for a bit, then stop it
+    test.sendAndExpect('obc.dfgm.dfgm_stop', {'err': 0})
+    test.send('obc.dfgm.dfgm_get_hk')
+
+    # Error checking
+    
+
+    test.summary() #call when done to print summary of tests
+
+
+if __name__ == '__main__':
+    testAllCommandsToOBC()
