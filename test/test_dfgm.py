43cdae9cbf1ffa9f5c25875de56ecb7b7298c11d
DanielSacro <59634352+DanielSacro@users.noreply.github.com>
2022-03-04 13:33:14 -0700

    Modified existing test cases and added new ones

--- test/test_dfgm.py
+++ test/test_dfgm.py
@@ -17,16 +17,22 @@ def testAllCommandsToOBC():
     test.send('obc.dfgm.dfgm_get_hk') # Update empty HK buffer by turning on DFGM for 1 sec
     time.sleep(2)
 
+    test.send('obc.dfgm.dfgm_get_hk') # Should instantly get updated data from the HK buffer
+
     test.sendAndExpect('obc.dfgm.dfgm_run(1)', {'err': 0})
     time.sleep(2)
 
     test.sendAndExpect('obc.dfgm.dfgm_run(10)', {'err': 0})
     time.sleep(11) 
 
+    test.sendAndExpect('obc.dfgm.dfgm_run(2147483647)', {'err': 0}) # INT_MAX
+    time.sleep(1)
+    test.sendAndExpect('obc.dfgm.dfgm_stop', {'err': 0}) # Check if DFGM actually stops dfgm_run
+
     test.sendAndExpect('obc.dfgm.dfgm_start', {'err': 0})
     time.sleep(5) # Let DFGM run for a bit
 
-    test.sendAndExpect('obc.dfgm.dfgm_stop', {'err': 0}) # Check if DFGM actually stops
+    test.sendAndExpect('obc.dfgm.dfgm_stop', {'err': 0}) # Check if DFGM actually stops dfgm_start
 
     test.send('obc.dfgm.dfgm_get_hk') # Check if data gets corrupt somehow (temp. should be ~20, not ~65000)
 
@@ -35,12 +41,15 @@ def testAllCommandsToOBC():
     test.sendAndExpect('obc.dfgm.dfgm_run(2147483648)', {'err': 1}) # Bad param. - Overflow for 32-bit int
     test.sendAndExpect('obc.dfgm.dfgm_run(0)', {'err': 1}) # Bad param. - Less than minRuntime
 
-    test.send('obc.dfgm.dfgm_run(100)')
+    test.sendAndExpect('obc.dfgm.dfgm_run(100)', {'err': 0})
     test.sendAndExpect('obc.dfgm.dfgm_start', {'err': 2}) # Busy - DFGM already running
-    test.send('obc.dfgm.dfgm_get_hk()') # Should not fail
-    test.send('obc.dfgm.dfgm_stop')
+    test.sendAndExpect('obc.dfgm.dfgm_run(1000)', {'err': 2}) # Busy
+    test.send('obc.dfgm.dfgm_get_hk()') # Should not fail during data collection
+    time.sleep(10)
+    test.send('obc.dfgm.dfgm_get_hk') # See if any of the values get updated from other packets
+    test.sendAndExpect('obc.dfgm.dfgm_stop', {'err': 0}) 
 
-    test.sendAndExpect('obc.dfgm.dfgm_stop', {'err': 0}) # Should not fail
+    test.sendAndExpect('obc.dfgm.dfgm_stop', {'err': 0}) # Should not fail if nothing is running
 
 
     test.summary() #call when done to print summary of tests
