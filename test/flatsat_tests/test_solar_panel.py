df88f8cddf8ea9741f94cf5776f13ae3bbce4571
Daniel Sacro <dbsacro@gmail.com>
2022-03-11 12:41:56 -0700

    Created basic script format for all FlatSat tests

---
+++ test/flatsat_tests/test_solar_panel.py
@@ -0,0 +1,30 @@
+'''
+ * Copyright (C) 2021  University of Alberta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+'''
+'''
+ * @file test_solar_panel.py
+ * @author Daniel Sacro
+ * @date 2022-3-10
+'''
+import time
+import numpy as np
+from testLib import testLib as test
+
+test = test() #call to initialize local test class
+
+def testAllCommandsToOBC():
+
+    test.summary() #call when done to print summary of tests
+
+if __name__ == '__main__':
+    testAllCommandsToOBC()
