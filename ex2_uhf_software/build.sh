dcfb2a0b569f74f8d13e82eabbba5bb2185398c5
joshdellaz <josh.lazaruk@gmail.com>
2022-05-13 08:44:29 -0700

    removed subtrees to fix commit history

--- ex2_uhf_software/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gcc -Iequipment_handler/include -Ihardware_interface/include -c -Wall -fpic equipment_handler/source/uTransceiver.c -o uTransceiver.o
-gcc -Iequipment_handler/include -Ihardware_interface/include -c -Wall -fpic equipment_handler/source/gnuradio_interface.c -o gnuradio_interface.o
-gcc -shared -o uTransceiver.so uTransceiver.o gnuradio_interface.o
-rm gnuradio_interface.o
-rm uTransceiver.o
--- ex2_uhf_software/install-hooks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git config core.hooksPath .git-hooks/hooks/
