0f10f5b3ee7a15ca48b624c058d774030008897a
Grace Yi <yunqing@ualberta.ca>
2022-06-09 14:06:11 -0600

    add adcs file uplink subservice

--- src/groundStation/system.py
+++ src/groundStation/system.py
@@ -3164,6 +3164,15 @@ class SystemValues(object):
                             }
                         }
                     },
+                    'ADCS_UPLOAD_FIRMWARE': {
+                        'subport': 110,
+                        'inoutInfo': {
+                            'args': None,
+                            'returns': {
+                                'err': '>b',    # missing firmware file: -2, calloc failed: -3
+                            }
+                        }
+                    },
                 }
             },
             'DFGM': {
