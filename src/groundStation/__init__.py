bf0b286008a124496e72e4f4884cb5d01a10e834
John Mabanta <johnmabanta51@gmail.com>
2022-06-27 16:09:42 -0600

    Updated imports so the website's comm.py can use cliGroundStation

--- src/groundStation/__init__.py
+++ src/groundStation/__init__.py
@@ -1,4 +1,5 @@
 try:
     from ex2_ground_station_software.src.groundStation import groundStation
+    from ex2_ground_station_software.src.groundStation import cliGroundStation
 except ModuleNotFoundError:
     pass
