9470b12c6f0d370c2e28c7f0d22f844313b91033
Robert Taylor <taylorr@ualberta.ca>
2022-07-21 13:52:21 -0600

    Add copyright notice

--- src/refactor/inputParser.py
+++ src/refactor/inputParser.py
@@ -1,3 +1,22 @@
+'''
+ * Copyright (C) 2022  University of Alberta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+'''
+'''
+ * @file inputParser.py
+ * @author Robert Taylor
+ * @date 2022-09-21
+'''
+
 from system import services, SatelliteNodes
 import re
 import numpy as np
