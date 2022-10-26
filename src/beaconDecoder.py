49f2c830f74a3262c627f02743a924b53b4490bd
joshdellaz <josh.lazaruk@gmail.com>
2022-10-26 12:03:19 -0600

    make more portable

--- src/beaconDecoder.py
+++ src/beaconDecoder.py
@@ -10,31 +10,34 @@ class beaconDecoder:
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect(("127.0.0.1", self.rxport))
         self.parser = ReceiveParser()
-    
+
+  
     def run(self):
-        while True:
-            self.rawdata = self.s.recv(10000)
-            print("----------Beacon Received:----------")
-            beacon_ID = 99
-            beacon_port = 1
-            payload_offset = 16
-            packet_num_offset = 4
-            decoded_data = base64.b64decode(self.rawdata)
-            decoded_data = bytearray(decoded_data)
-            if decoded_data[packet_num_offset] == 1:
-                decoded_data[:0] = (1).to_bytes(1,'big')
-                ret = self.parser.parseReturnValue(beacon_ID, beacon_port, decoded_data)
-            elif decoded_data[packet_num_offset] == 2:
-                decoded_data[:0] = (2).to_bytes(1,'big')
-                ret = self.parser.parseReturnValue(beacon_ID, beacon_port, decoded_data)
-            else:
-                print('Received beacon with invalid packet number: ' + str(decoded_data[packet_num_offset]))          
-            if ret:
-                for key, value in ret.items():
-                    print("{} : {}".format(key, value))
-                print("")
+        self.rawdata = self.s.recv(10000)
+        beacon_ID = 99
+        beacon_port = 1
+        payload_offset = 16
+        packet_num_offset = 4
+        decoded_data = base64.b64decode(self.rawdata)
+        decoded_data = bytearray(decoded_data)
+        if decoded_data[packet_num_offset] == 1:
+            decoded_data[:0] = (1).to_bytes(1,'big')
+            return self.parser.parseReturnValue(beacon_ID, beacon_port, decoded_data)
+        elif decoded_data[packet_num_offset] == 2:
+            decoded_data[:0] = (2).to_bytes(1,'big')
+            return self.parser.parseReturnValue(beacon_ID, beacon_port, decoded_data)
+        else:
+            return None
 
 
 if __name__ == '__main__':
     decoder = beaconDecoder()
-    decoder.run()   
+    while True:
+        beacon = decoder.run() 
+        print("----------Beacon Received:----------")
+        if beacon:
+            for key, value in beacon.items():
+                print("{} : {}".format(key, value))
+        else:
+            print('Received beacon with invalid packet number...')          
+        print("")
