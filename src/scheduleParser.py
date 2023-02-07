1cc9dba0410499bd384277abd082c4e9a79c0829
Ron Unrau <rcunrau@ualberta.ca>
2023-02-07 16:07:06 -0700

    scheduleParser.py: support wildcards

--- src/scheduleParser.py
+++ src/scheduleParser.py
@@ -37,7 +37,8 @@ class ScheduleParser:
     def __init__(self, cmdList = None):
         self.cmdList = cmdList
         self.cmds = []
-        self.now = []
+        self.now = None
+        self.listnow = []
         self.repeat = 0
 
     def _splitdt(self, dt: datetime):
@@ -72,28 +73,52 @@ class ScheduleParser:
             self.cmds.append(self._parseCmd(cron))
 
     def _parseWildcard(self, index: int, field: str):
-        if not self.now:
-            # lazy initialization
-            self.now = datetime.now(timezone.utc)
-            listnow = self._splitdt(self.now)
-
         if self.repeat != 0:
-            return listnow[index]
+            return self.listnow[index]
 
         # Limitations: we don't support wildcard milliseconds, and if you
         # specify month as a wildcard we assume all months contain 30 days.
         # If this turns out to be a problem we can figure something out.
         periods = [0, 1, 60, 60*60, 24*60*60, 30*24*60*60, 0]
 
+        first = self.listnow[index]
         slash = field.find('/')
         if slash == -1:
             self.repeat = periods[index]
         else:
-            self.repeat = int(field[slash+1:])*periods[index]
-
-        return listnow[index]
+            print("step in index {}: {}".format(index, field))
+            step = int(field[slash+1:])
+            self.repeat = step*periods[index]
+            # To calculate first, move to the next step multiple.
+            # For example, if field=7 and step=5, bump field to 10
+            rem = first % step
+            if rem > 0:
+                maxs = [999, 59, 59, 23, 31, 2023]
+                print("now[{}]={}, step={}, new={}".format(index, first, step,
+                                                           first+step-rem))
+                first = first + (step - rem)
+                if first > maxs[index]:
+                    # The tricky part about bumping a field is that it may
+                    # need to wrap and overflow to the next field. And then
+                    # the next field could overflow, etc. The most obvious
+                    # example is incrementing the seconds field at 23:59:59.
+                    print("overflow {}, at index {}".format(first, index))
+                    self.listnow[index] = first
+                    for i in range(index,6):
+                        if self.listnow[i] > maxs[i]:
+                            print("overflow at {}: {}".format(i, self.listnow[i]))
+                            self.listnow[i] -= maxs[i] + 1
+                            self.listnow[i+1] += 1
+                            if i >= 3:
+                                print("Warning: overflow into day/month {}".format(field))
+                    first = self.listnow[index]
+        return first
 
     def parseCmd(self, cronspec: str):
+        self.now = datetime.now(timezone.utc)
+        self.listnow = self._splitdt(self.now)
+        self.repeat = 0
+
         cmd = {}
         cmdfields = cronspec.split()
         timefields = []
@@ -117,9 +142,22 @@ class ScheduleParser:
                                second=timefields[1],
                                microsecond=timefields[0]*1000,
                                tzinfo=timezone.utc)
+
         cmd['first'] = calendar.timegm(nextdt.timetuple())
+        if nextdt < self.now:
+            # Particularly for periodic commands, it's possible that first has
+            # already passed. For example, for "0 0 */5 * * *" it's possible we
+            # calculated first to be 11:05:00, and it's already 11:07:00. Just
+            # add the period and we should get 11:10:00.
+            print("Warning: cron spec {} < now {}".format(nextdt, self.now))
+            if self.repeat > 0:
+               cmd['first'] += self.repeat
+               print("Adjusting first by {} sec to {}".format(self.repeat,
+                             datetime.fromtimestamp(cmd['first'], timezone.utc)))
+
         cmd['repeat'] = self.repeat
         cmd['last'] = 0
         cmd['op'] = " ".join(cmdfields[7:])
+
         print("cmd: {}".format(cmd))
         return cmd
