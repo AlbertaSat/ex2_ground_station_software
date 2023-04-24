33127f0e407ba235145dbdbfb9d72a44990a564a
joshdellaz <josh.lazaruk@gmail.com>
2023-04-24 06:37:44 -0600

    beacondecoder: parse and decode switch statuses
    Signed-off-by: joshdellaz <josh.lazaruk@gmail.com>

--- src/beaconDecoder.py
+++ src/beaconDecoder.py
@@ -11,7 +11,7 @@ class beaconDecoder:
         self.s.connect(("127.0.0.1", self.rxport))
         self.parser = ReceiveParser()
 
-  
+
     def run(self):
         self.rawdata = self.s.recv(10000)
         beacon_ID = 99
@@ -22,22 +22,36 @@ class beaconDecoder:
         decoded_data = bytearray(decoded_data)
         if decoded_data[packet_num_offset] == 1:
             decoded_data[:0] = (1).to_bytes(1,'big')
-            return self.parser.parseReturnValue(beacon_ID, beacon_port, decoded_data)
+            self.beacon = self.parser.parseReturnValue(beacon_ID, beacon_port, decoded_data)
+            self.unpack_switch_status()
         elif decoded_data[packet_num_offset] == 2:
             decoded_data[:0] = (2).to_bytes(1,'big')
-            return self.parser.parseReturnValue(beacon_ID, beacon_port, decoded_data)
+            self.beacon = self.parser.parseReturnValue(beacon_ID, beacon_port, decoded_data)
         else:
-            return None
+            self.beacon = None
+
+        return self.beacon
+
+    def unpack_switch_status(self)
+        switches = ['Starboard Panel Switch', '2U Payload Switch', 'Port Panel Switch' \
+                    'UHF Nadir Switch', 'UHF Starboard Switch', 'UHF Zenith Switch', \
+                    'UHF Port Switch', 'DFGM Switch']
+
+        shift_bits = 0
+        for switch in switches:
+            status = self.beacon['switch_stat'] >> shift_bits) & 1
+            shift_bits += 1
+            self.beacon.update({switch: status})
 
 
 if __name__ == '__main__':
     decoder = beaconDecoder()
     while True:
-        beacon = decoder.run() 
+        beacon = decoder.run()
         print("----------Beacon Received:----------")
         if beacon:
             for key, value in beacon.items():
                 print("{} : {}".format(key, value))
         else:
-            print('Received beacon with invalid packet number...')          
+            print('Received beacon with invalid packet number...')
         print("")
